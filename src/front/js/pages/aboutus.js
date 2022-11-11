0791d2d0140f9bd195ae762d9dacf3ee030796e3
Juan1733 <juan.araque@correo.unimet.edu.ve>
2022-11-11 01:41:28 +0000

    detalles de aboutus

--- src/front/js/pages/aboutus.js
+++ src/front/js/pages/aboutus.js
@@ -15,11 +15,11 @@ import juan from '../../img/juan.jpeg'
 function AboutUs() {
   return (
 <>   
-<div className='container-fluid aboutus justify-content-center' style={{backgroundImage: `url(${img})`}}>
+<div className='container-fluid aboutus justify-content-center pb-5' style={{backgroundImage: `url(${img})`}}>
   <div className="d-flex container-fluid upperhero" lc-helper="background" >
     .
   </div>
-  <div className="container p-3 textbox justify-content-center align-items-center rounded" style={{backgroundColor:"rgb(226 226 226)"}}>
+  <div className="container p-3 py-5 textbox justify-content-center align-items-center">
     <div className="row">
       <div className="col-md-4 text-center align-self-center">
         <div className="lc-block border-end border-2 border-dark ">
@@ -28,7 +28,7 @@ function AboutUs() {
           </div>
         </div>
       </div>
-      <div className="col-md-8">
+      <div className="col-md-8 row align-items-center">
         <div className="lc-block ">
           <div editable="rich">
             <p className="display-5">Impulsando los eventos para todos</p>
@@ -49,33 +49,37 @@ function AboutUs() {
   </div>
 </div>
 <div className='scrolldown1 container-fluid py-5' >
-  <div className='display-3 justify-content-center d-flex border-bottom border-warning '>
-    Programadores:
+  <div className='display-3 justify-content-center d-flex border-bottom border-warning pb-5'>
+    Los Programadores
     
   </div>
   <div className='d-flex flex-wrap justify-content-around mt-5'>
-    <div className='d-flex flex-wrap images justify-content-center'>
-      <img src={joel} className='h-5 images'>
+    <div className='d-flex flex-wrap images-per-con mb-3 justify-content-center'>
+      <img src={juan} className='h-5 images-person mb-3 juan'>
       </img>
-      <p className='text d-flex align-items-end '>Joel Leonardo Medina, ingeniero en sistemas, front end developer.</p>
+      <h3>Juan Araque</h3>
+      <h5 className='text-center d-flex'>Estudiante de ingeniería en sistemas, 19 años.</h5>
+      
     </div>
-    <div className='d-flex flex-wrap images justify-content-center'>
-      <img src={tito} className='h-5 images'>
+    <div className='d-flex flex-wrap images-per-con mb-3 justify-content-center'>
+      <img src={joel} className='h-5 images-person mb-3 joel'>
       </img>
-      <p className='text d-flex align-items-end '>Tito Ángel Balza, estudiante de economía empresarial, 19 años.</p>
+      <h3>Joel Leonardo Medina</h3>
+      <h5 className='text-center d-flex'>Ingeniero en sistemas, front end developer, 34 años.</h5>
     </div>
-    <div className='d-flex flex-wrap images justify-content-center'>
-      <img src={juan} className='h-5 images'>
+    <div className='d-flex flex-wrap images-per-con mb-3 justify-content-center'>
+      <img src={tito} className='h-5 images-person tito mb-3'>
       </img>
-      <p className='text d-flex align-items-end '>Juan Araque, estudiante de ingeniería en sistemas, 19 años.</p>
+      <h3>Tito Ángel Balza</h3>
+      <h5 className='text-center d-flex'>Estudiante de economía empresarial, 19 años.</h5>
     </div>
   </div>
 </div>
 <div className='scrolldown2 container-fluid py-5' >
-    <div className='display-3 justify-content-center d-flex border-bottom border-warning '>
+    <div className='display-3 justify-content-center d-flex border-bottom border-warning pb-5'>
     ¿Cómo lo hicimos?
     </div>
-    <p className='justify-content-center d-flex fs-4 text mt-2 ps-2 pe-2'> 
+    <p className='justify-content-center d-flex fs-4 text mt-2 ps-2 pe-2 pt-3'> 
     Para hacer esta página usamos distintos lenguajes de programación y/o librerías:
     </p>
     <div className='d-flex flex-wrap justify-content-around mt-5'>
